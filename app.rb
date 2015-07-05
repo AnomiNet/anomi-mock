bc1f557464534c91305b0d474e71e96e24df15e1
Ayumi Yu <ayumi@ayumiyu.com>
2015-07-04 18:05:50 -0700

    POST /posts/

--- app.rb
+++ app.rb
@@ -6,6 +6,19 @@ helpers do
   def request_body
     @request_body ||= Yajl::Parser.parse( request.body )
   end
+
+  def require_params(required_params)
+    missing_attrs = required_params - request_body.keys
+    if missing_attrs.any?
+      { error: "Required params missing: #{ missing_attrs.join(', ') }" }
+    else
+      {}
+    end
+  end
+
+  def as_json(obj)
+    Yajl::Encoder.encode(obj)
+  end
 end
 
 get '/' do
@@ -17,11 +30,25 @@ end
 post '/users/' do
   content_type :json
 
-  if request_body['handle'].empty?
-    status 400
-    return { error: '"handle" param is required' }
-  end
+  errors = require_params( %w(handle) )
+  status 400 and return( as_json(errors) ) if errors.any?
 
   status 201
   File.read("fixtures/create_user.json")
+end
+
+
+# Post #create
+post '/posts/' do
+  content_type :json
+
+  errors = require_params( %w(body tldr) )
+  status 400 and return( as_json(errors) ) if errors.any?
+
+  status 201
+  if request_body['parent_id']
+    File.read("fixtures/create_post_reply.json")
+  else
+    File.read("fixtures/create_post_root.json")
+  end
 end
